949c1da6e840a8a9dde8e60d36eef4e068250ead
Fernando Poumián <fpoumian@gmail.com>
2018-02-28 19:41:26 -0700

    Add factory.js and web3.js

---
+++ ethereum/factory.js
@@ -0,0 +1,7 @@
+import web3 from './web3'
+import CampaignFactory from './build/CampaignFactory.json'
+
+const instance = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  '0xCbb8201ba1EE6d9969DAD07d2A5C07B4BdcEFd31'
+)
