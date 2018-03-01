ca37c1633e65ba800fb0ae0f255c8713d0bb19aa
Fernando Poumián <fpoumian@gmail.com>
2018-02-28 18:25:34 -0700

    Add more tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -21,7 +21,6 @@ beforeEach(async () => {
     from: accounts[0],
     gas: '1000000',
   })
-
   ;[campaignAddress] = await factory.methods.getDeployedCampaigns().call()
   campaign = await new web3.eth.Contract(
     JSON.parse(compiledCampaign.interface),
@@ -33,5 +32,45 @@ describe('Campaigns', () => {
   it('deploys a factory and a campaign', () => {
     assert.ok(factory.options.address)
     assert.ok(campaign.options.address)
+  })
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call()
+    assert.equal(accounts[0], manager)
+  })
+
+  it('allows people to contribute money and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: accounts[1],
+    })
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call()
+    assert(isContributor)
+  })
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await campaign.methods.contribute().send({
+        value: '5',
+        from: accounts[1],
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+  })
+
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy batteries', '100', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      })
+
+    const request = await campaign.methods.requests(0).call()
+
+    assert.equal('Buy batteries', request.description)
   })
 })
