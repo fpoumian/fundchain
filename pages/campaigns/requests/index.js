7ec831b32a0dc1526c8409ca065f3c67acc620b0
Fernando Poumián <fpoumian@gmail.com>
2018-03-02 12:48:38 -0700

    Create component and add route for New Request page

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+import Layout from '../../../components/Layout'
+import { Link } from '../../../routes'
 
 class RequestIndex extends React.Component {
+  static getInitialProps({ query: { address } }) {
+    return { address }
+  }
+
   render() {
-    return(
-      <h3>{`Request List`}</h3>
+    return (
+      <Layout>
+        <h3>{`Request List`}</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>{`Add Request`}</Button>
+          </a>
+        </Link>
+      </Layout>
     )
   }
 }
