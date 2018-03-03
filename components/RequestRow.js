afe00de33ce7a2cb55a1eb6f95d8b14324a776f2
Fernando Poumián <fpoumian@gmail.com>
2018-03-02 17:58:51 -0700

    Add RequestRow component

---
+++ components/RequestRow.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { Table, Button } from 'semantic-ui-react'
+import web3 from '../ethereum/web3'
+import PropTypes from 'prop-types'
+
+import Campaign from '../ethereum/campaign'
+
+class RequestRow extends React.Component {
+  onApprove = async () => {
+    const campaign = Campaign(this.props.address)
+    const accounts = await web3.eth.getAccounts()
+    await campaign.methods.approveRequest(this.props.id).send({
+      from: accounts[0],
+    })
+  }
+
+  onFinalize = async () => {
+    const campaign = Campaign(this.props.address)
+    const accounts = await web3.eth.getAccounts()
+    await campaign.methods.finalizeRequest(this.props.id).send({
+      from: accounts[0],
+    })
+  }
+
+  render() {
+    const {
+      id,
+      request: { description, value: amount, recipient, approvalCount },
+      approversCount,
+    } = this.props
+    return (
+      <Table.Row>
+        <Table.Cell>{id}</Table.Cell>
+        <Table.Cell>{description}</Table.Cell>
+        <Table.Cell>{web3.utils.fromWei(amount, 'ether')}</Table.Cell>
+        <Table.Cell>{recipient}</Table.Cell>
+        <Table.Cell>
+          {approvalCount}/{approversCount}
+        </Table.Cell>
+        <Table.Cell>
+          <Button color={`green`} basic onClick={this.onApprove}>
+            {`Approve`}
+          </Button>
+        </Table.Cell>
+        <Table.Cell>
+          <Button color={`teal`} basic onClick={this.onFinalize}>
+            {`Finalize`}
+          </Button>
+        </Table.Cell>
+      </Table.Row>
+    )
+  }
+}
+
+RequestRow.propTypes = {}
+
+export default RequestRow
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Button } from 'semantic-ui-react'
+import { Button, Table } from 'semantic-ui-react'
 
 import Layout from '../../../components/Layout'
 import { Link } from '../../../routes'
 import Campaign from '../../../ethereum/campaign'
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends React.Component {
   static async getInitialProps({ query: { address } }) {
     const campaign = Campaign(address)
     const requestCount = await campaign.methods.getRequestsCount().call()
+    const approversCount = await campaign.methods.approversCount().call()
 
     const requests = await Promise.all(
       Array(parseInt(requestCount))
@@ -16,10 +18,11 @@ class RequestIndex extends React.Component {
         .map((element, index) => campaign.methods.requests(index).call())
     )
 
-    return { address, requests, requestCount }
+    return { address, requests, requestCount, approversCount }
   }
 
   render() {
+    const { Header, Row, HeaderCell, Body } = Table
     return (
       <Layout>
         <h3>{`Request List`}</h3>
@@ -28,6 +31,32 @@ class RequestIndex extends React.Component {
             <Button primary>{`Add Request`}</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>{`ID`}</HeaderCell>
+              <HeaderCell>{`Description`}</HeaderCell>
+              <HeaderCell>{`Amount`}</HeaderCell>
+              <HeaderCell>{`Recipient`}</HeaderCell>
+              <HeaderCell>{`Approval Count`}</HeaderCell>
+              <HeaderCell>{`Approve`}</HeaderCell>
+              <HeaderCell>{`Finalize`}</HeaderCell>
+            </Row>
+          </Header>
+          <Body>
+          {
+            this.props.requests.map((request, index) => (
+              <RequestRow
+                request={request}
+                approversCount={this.props.approversCount}
+                key={index}
+                id={index}
+                address={this.props.address}
+              />
+            ))
+          }
+          </Body>
+        </Table>
       </Layout>
     )
   }
