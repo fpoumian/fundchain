1a845d84c21d9624066928f6eba6f60b35a7787a
Fernando Poumián <fpoumian@gmail.com>
2018-03-02 20:53:27 -0700

    Fix Campaign contract so that it doesn't increase approversCount more than once

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json'
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x7e26c7a715C4eabB7AA6BdFb3dA0c0Fa1532AFD6'
+  '0xF0a100b6540E716E4d0AeFeE06Ac17C233AF9dCA'
 )
 
 export default instance
